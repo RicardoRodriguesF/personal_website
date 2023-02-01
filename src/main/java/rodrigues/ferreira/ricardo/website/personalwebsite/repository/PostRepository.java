a0bcf28e817a78bcbb05c6bdbd0bfcdc441f56b9
rrodferr <ricardo.rodriguesferreira@everis.nttdata.com>
2023-02-01 12:07:54 +0000

    Revert "Created Post and Comment Entities"
    This reverts commit 77e32e866f4ae294072bddf0c255c3be559611cb.

--- src/main/java/rodrigues/ferreira/ricardo/website/personalwebsite/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package rodrigues.ferreira.ricardo.website.personalwebsite.repository;
-
-import org.springframework.stereotype.Repository;
-import rodrigues.ferreira.ricardo.website.personalwebsite.entity.Post;
-
-@Repository
-public interface PostRepository extends BaseRepository<Post> {
-}
